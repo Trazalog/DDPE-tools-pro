872d83d320a56e3c0b441161ef504b93175b3e5b
Hugo Gallardo <hugoeduardogallardo@gmail.com>
2021-08-24 16:17:26 -0300

    reporte historico de camiones

--- reports/historico_camiones/Historico_camiones.view.php
+++ reports/historico_camiones/Historico_camiones.view.php
@@ -40,7 +40,7 @@
                 </div>
               </div>
 
-              <!-- <div class="col-md-4 col-md-6 mb-4 mb-lg-0">
+              <div class="col-md-4 col-md-6 mb-4 mb-lg-0">
                 <label style="padding-left: 20%;">Fecha Hasta <strong class="text-danger">*</strong> :</label>
                 <div class="input-group date">
                   <a class="input-group-addon" id="daterange-btn" title="Más fechas">
@@ -49,18 +49,7 @@
                   </a>
                   <input type="date" class="form-control pull-right" id="datepickerHasta" name="datepickerHasta" placeholder="Hasta">
                 </div>
-              </div> -->
-
-              <div class="col-md-4 col-md-6 mb-4 mb-lg-0">
-                <label for="datepickerHasta">Fecha hasta:</label>
-                <div class="input-group">
-                    <div class="input-group-addon">
-                        <i class="fa fa-calendar"></i>
-                    </div>
-                    <input type="text" class="form-control pull-right datepicker" id="datepickerHasta" name="datepickerHasta" placeholder="Seleccione fecha">
-                </div>
               </div>
-
 
             </div>
 
@@ -109,8 +98,8 @@
 
             <div class="col-md-4 col-md-6 mb-4 mb-lg-0">
               <label for="resultado" class="form-label">Resultado:</label>
-              <?php  echo selectFromCore('resultado', 'Seleccione Resultado', '888-888-tipos_resultado', '') ?>
-              </select>
+              <?php  echo selectFromCore('resultado', 'Seleccione Resultado', '888-tipos_resultado', '') ?>
+
             </div>
 
           </div>
@@ -154,6 +143,12 @@
             // "showHeader" => false,
 
             "columns" => array(
+                array(
+                  "label" => "Ver",
+                  "value" => function() {
+                    return '<i class="fa fa-search text-light-blue"  style="cursor: pointer;margin: 3px;" title="Ver Pedido" onclick=""></i>';
+                  }
+                ),
                 "case_id" => array(
                   "label" => "Nº Insp"
                 ),
@@ -194,7 +189,16 @@
             "cssClass" => array(
               "table" => "table-scroll table-responsive dataTables_wrapper form-inline dt-bootstrap dataTable table table-bordered table-striped table-hover display",
               "th" => "sorting"
-            )
+            ),
+            "paging"=>array(
+              "pageSize"=>10,
+              "pageIndex"=>0,
+            ),
+            "options"=>array(
+              "searching"=>true
+            ),
+            "searchOnEnter" => true,
+            "searchMode" => "or"
           ));
           ?>
         </div>
@@ -202,9 +206,8 @@
 
       <!--_______ BOTONES _______-->
         <div id="acciones" class="" style="float: right !important;">
-        <button type="button" class="btn btn-success" onclick="detaReporte()">Detalle</button>
-          <button type="button" class="btn btn-success" onclick="exportarPDF()">Imprimir</button>
-          <button type="button" class="btn btn-primary" onclick="exportarExcel()">Exportar a Excel</button>
+          <!-- <button type="button" class="btn btn-success" onclick="exportarPDF()">Imprimir</button>
+          <button type="button" class="btn btn-primary" onclick="exportarExcel()">Exportar a Excel</button> -->
           <button type="button" class="btn btn-primary" onclick="filtrar()">FILTRAR</button>
         </div>
       <!--_______ BOTONES _______-->
@@ -216,174 +219,185 @@
 </div>
 
 <script>
-  // llena selects empresas
-  // $('.empresa').select2({
-  //     ajax: {
-  //         url: "<?php //echo SICP; ?>inspeccion/buscaEmpresas",
-  //         dataType: 'json',
-  //         delay: 250,
-  //         data: function (params) {
-  //             return {
-  //                 patron: params.term, // parámetro búsqueda
-  //                 page: params.page
-  //             };
-  //         },
-  //         processResults: function (data, params) {
+  // Llena selects empresas
+    $('.empresa').select2({
+        ajax: {
+            url: "<?php echo SICP; ?>inspeccion/buscaEmpresas",
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return {
+                    patron: params.term, // parámetro búsqueda
+                    page: params.page
+                };
+            },
+            processResults: function (data, params) {
 
-  //             params.page = params.page || 1;
-              
-  //             var results = [];
-  //             $.each(data, function(i, obj) {
-  //                 results.push({
-  //                     id: obj.cuit,
-  //                     text: obj.razon_social,
-  //                 });
-  //             });
-  //             return {
-  //                 results: results,
-  //                 pagination: {
-  //                     more: (params.page * 30) < results.length
-  //                 }
-  //             };
-  //         }
-  //     },
-  //     placeholder: 'Buscar empresa',
-  //     minimumInputLength: 3,
-  //     templateResult: function (empresa) {
-
-  //         if (empresa.loading) {
-  //             return "Buscando empresas...";
-  //         }
-
-  //         var $container = $(
-  //             "<div class='select2-result-repository clearfix'>" +
-  //             "<div class='select2-result-repository__meta'>" +
-  //                 "<div class='select2-result-repository__title'></div>" +
-  //                 "<div class='select2-result-repository__description'></div>" +
-  //             "</div>" +
-  //             "</div>"
-  //         );
-
-  //         $container.find(".select2-result-repository__title").text(empresa.id);
-  //         $container.find(".select2-result-repository__description").text(empresa.text);
-
-  //         return $container;
-  //     },
-  //     templateSelection: function (empresa) {
-  //         return empresa.text + ' ' + empresa.id + '';
-  //     },
-  //     language: {
-  //         noResults: function() {
-  //             return '<option>No hay coincidencias</option>';
-  //         },
-  //         inputTooShort: function () {
-  //             return 'Ingrese 3 o mas dígitos para comenzar la búsqueda'; 
-  //         }
-  //     },
-  //     escapeMarkup: function(markup) {
-  //         return markup;
-  //     },
-  // });
-
-  // llena tabla con la informacion filtrada
-  function filtrar() {
-
-    var data = buscaDatos();
-    if (!data) {
-      alert('Seleccione fechas por favor...');
-      return;
-    }
-    $.ajax({
-        type: 'POST',
-        data:{data},
-        url: "<?php echo SICP; ?>reportes/historicoCamiones",
-        success: function(result) {
-
+                params.page = params.page || 1;
+                
+                var results = [];
+                $.each(data, function(i, obj) {
+                    results.push({
+                        id: obj.cuit,
+                        text: obj.razon_social,
+                    });
+                });
+                return {
+                    results: results,
+                    pagination: {
+                        more: (params.page * 30) < results.length
+                    }
+                };
+            }
         },
-        error: function(result){
-                  
+        placeholder: 'Buscar empresa',
+        minimumInputLength: 3,
+        templateResult: function (empresa) {
+
+            if (empresa.loading) {
+                return "Buscando empresas...";
+            }
+
+            var $container = $(
+                "<div class='select2-result-repository clearfix'>" +
+                "<div class='select2-result-repository__meta'>" +
+                    "<div class='select2-result-repository__title'></div>" +
+                    "<div class='select2-result-repository__description'></div>" +
+                "</div>" +
+                "</div>"
+            );
+
+            $container.find(".select2-result-repository__title").text(empresa.id);
+            $container.find(".select2-result-repository__description").text(empresa.text);
+
+            return $container;
         },
-        complete: function(){
-                  
-        }
+        templateSelection: function (empresa) {
+            return empresa.text + ' ' + empresa.id + '';
+        },
+        language: {
+            noResults: function() {
+                return '<option>No hay coincidencias</option>';
+            },
+            inputTooShort: function () {
+                return 'Ingrese 3 o mas dígitos para comenzar la búsqueda'; 
+            }
+        },
+        escapeMarkup: function(markup) {
+            return markup;
+        },
     });
-  }
 
-  function buscaDatos() {
-
-    desde = $("#datepickerDesde").val();
-    hasta = $("#datepickerHasta").val();
-    if (desde == "" || hasta == "") {
-      return false;
+  // Llena tabla con la informacion filtrada
+    function filtrar() {
+      wo();
+      var data = buscaDatos();
+      if (!data) {
+        alert('Seleccione fechas por favor...');
+        return;
+      }
+      $.ajax({
+          type: 'POST',
+          data:{data},
+          url: "<?php echo SICP; ?>reportes/historicoCamiones",
+          success: function(result) {
+                $('#reportContent').empty();
+                $('#reportContent').html(result);
+                wc();
+          },
+          error: function(result){
+                    
+          },
+          complete: function(){
+                    
+          }
+      });
     }
-    origen = $("#origen").val();
-    //if (origen == null) {
-      origen = 'TODOS';
-    //}
-    destino = $("#destino").val();
-    //if (destino == null) {
-      destino = 'TODOS';
-    //}
-    transportista = $("#transportista").val();
-    //if (transportista == null) {
-      transportista = 'TODOS';
-    //}
-    resultado = $("#resultado>option:selected").val();
-    if (resultado == 0) {
-      resultado = 'TODOS';
+
+  // Recolecta los datos de los filtros
+    function buscaDatos() {
+
+      desde = $("#datepickerDesde").val();
+      hasta = $("#datepickerHasta").val();
+      if (desde == "" || hasta == "") {
+        return false;
+      }
+      origen = $("#origen").val();
+      if (origen == null) {
+        origen = 'TODOS';
+      }
+      destino = $("#destino").val();
+      if (destino == null) {
+        destino = 'TODOS';
+      }
+      transportista = $("#transportista").val();
+      if (transportista == null) {
+        transportista = 'TODOS';
+      }
+      resultado = $("#resultado>option:selected").val();
+      if (resultado == 0) {
+        resultado = 'TODOS';
+      }
+      producto = $("#producto>option:selected").val();
+      if (producto == 0) {
+        producto = 'TODOS';
+      }
+      var data = {};
+      data.fec_desde = desde;
+      data.fec_hasta = hasta;
+      data.cuit_origen = origen;
+      data.cuit_destino = destino;
+      data.cuit_transporte = transportista;
+      data.resultado = resultado;
+      data.tipo_producto = producto;
+
+      return data;
     }
-    producto = $("#producto>option:selected").val();
-    if (producto == 0) {
-      producto = 'TODOS';
+  // Levanta modal detalle de reporte
+    $(document).on("click", ".fa-search", function() {
+
+      let case_id = $(this).parents("tr").find("td").eq(1).html();
+      let caseId = case_id.trim();
+      var data = {};
+      data.caseId = caseId;
+      $("#modalBodyDetalle").load("<?php echo base_url(SICP); ?>reportes/detaReporte", data);
+      $("#modalDetalle").modal('show');
+
+    });
+
+  // Muestra el thumbnail en tamaño grande
+    function preview(imgs) {
+      // Tomo el elemento para la vista previa
+      var expandImg = document.getElementById("expandedImg");
+      // Le asigno la misma src al elemento de la vista previa
+      expandImg.src = imgs.src;
     }
-    var data = {};
-    data.fec_desde = desde;
-    data.fec_hasta = hasta;
-    data.cuit_origen = origen;
-    data.cuit_destino = destino;
-    data.cuit_transporte = transportista;
-    data.resultado = resultado;
-    data.tipo_producto = producto;
 
-    return data;
-  }
-
-  fechaMagic();
-  // levanta modal detalle de reporte
-  function detaReporte() {
-    //FIXME: QUITAR CONFIGURACION EN CODIGO JEJE
-    var data = {};
-    data.caseId = "13077";
-    $("#modalBodyDetalle").load("<?php echo base_url(SICP); ?>reportes/detaReporte", data);
-    $("#modalDetalle").modal('show');
-  }
-
-  //Exporta a Excel
+  // Exporta a Excel
     // function exportarExcel(){
 
-      //window.open("<?php echo base_url(ALM); ?>Reportes/excelTest?fec1="+fec1+"&fec2="+fec2+"&depo="+depo+"&arti="+arti+"&tpoArt="+tpoArt+"&estado="+estado);
+      //window.open("<?php //echo base_url(ALM); ?>Reportes/excelTest?fec1="+fec1+"&fec2="+fec2+"&depo="+depo+"&arti="+arti+"&tpoArt="+tpoArt+"&estado="+estado);
     // }
 
-  //Imprimir ACTA
-  function imprimirActa(){
-    var base = "<?php echo base_url()?>";
-    $('#actaInfraccion').printThis({
-        debug: false,
-        importCSS: true,
-        importStyle: true,
-        loadCSS: "",
-        base: base,
-        pageTitle : "TRAZALOG TOOLS",
-    });
-  }
+  // Imprimir ACTA
+    function imprimirActa(){
+      var base = "<?php// echo base_url()?>";
+      $('#actaInfraccion').printThis({
+          debug: false,
+          importCSS: true,
+          importStyle: true,
+          loadCSS: "",
+          base: base,
+          pageTitle : "TRAZALOG TOOLS",
+      });
+    }
 
   // cierra modal detalle de reporte
-  function cerrarModal(){
-    $("#modalDetalle").modal('hide');
-  }
+    function cerrarModal(){
+      $("#modalDetalle").modal('hide');
+    }
 
 </script>
-
 
 <div class='modal fade bs-example-modal-lg' id='modalDetalle' tabindex='-1' role='dialog' aria-labelledby='myLargeModalLabel'>
   <div class='modal-dialog modal-lg' role='document'>
