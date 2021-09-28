f6e1e3910d90fec7df6c7d01c0380834f5ce59b1
Rogelio Sanchez <rnsanchez@trazalog.com>
2021-09-28 11:41:30 -0300

    Corrección modales responsive

--- views/ingresoBarrera/listar_ingresos_barrera.php
+++ views/ingresoBarrera/listar_ingresos_barrera.php
@@ -99,11 +99,15 @@
 ?>
 <!-- The Modal -->
 <div class="modal modal-fade" id="mdl-vista">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="xmodal-body">
                 <?php $this->load->view(SICP.'ingresoBarrera/mdl_ingreso_detalle', $data); ?>
             </div>
+            <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn" data-dismiss="modal">Cerrar</button>
+          </div>
         </div>
     </div>
 </div>
